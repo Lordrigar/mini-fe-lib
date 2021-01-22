112aa7ba471a0fd73229473e71c23a1526dabd07
lordrigar <wojciechzaorski@gmail.com>
2021-01-22 21:08:24 +0100

    Setup webpack and dev server

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vanilla JS App for SpaceX API
+
+Goal is to create SPA with Real Time data fetching without use of frameworks.
+
+App features:
+
+- Local and global state (event based)
+- Re-rendering of only those components that require it
---
+++ webpack.dev.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, './dist/'),
+    open: true,
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+  },
+});
